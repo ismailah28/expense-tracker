a5c1309a8d4f4736f9288192b21081521292fd48
Ismaila Hassan <ismailah28@gmail.com>
2020-02-28 17:41:30 +0100

    Scaffold vue app with tailwindcss and purgecss

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-2xl text-blue-800 font-bold">
+    Hello World!
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
