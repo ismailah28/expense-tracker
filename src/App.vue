fdbfc43ca87099827b58cdde1847c6c9de496994
Ismaila Hassan <ismailah28@gmail.com>
2020-02-29 02:58:03 +0100

    Add Balance component and fix responsive padding

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
-  <div id="app" class="flex flex-col max-w-md min-h-screen mx-auto">
+  <div
+    id="app"
+    class="flex flex-col max-w-md min-h-screen mx-auto px-4 sm:px-0"
+  >
     <Header msg="Expense Tracker" />
+    <Balance />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Balance from "./components/Balance";
 import "./main.css";
 
 export default {
   name: "App",
   components: {
-    Header
+    Header,
+    Balance
   }
 };
 </script>
---
+++ src/components/Balance.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="my-8">
+    <h4 class="text-base uppercase font-medium">Your Balance</h4>
+    <h1 class="text-4xl font-bold">$0.00</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "Balance"
+};
+</script>
