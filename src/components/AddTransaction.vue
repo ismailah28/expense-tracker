7ad9f64872012784bad18eeea066a7bbab5f1193
Ismaila Hassan <ismailah28@gmail.com>
2020-03-05 12:43:50 +0100

    Add validation to form
    Also refactored code

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -3,16 +3,18 @@
     <h3 class="border-b-2 pb-2 mt-4 text-h3 font-medium">
       Add New Transaction
     </h3>
-    <form @submit.prevent="addTransaction">
+    <form @submit.prevent="addTransaction" novalidate>
       <div class="">
         <label class="my-4 inline-block font-medium" for="text">Text</label>
         <input
-          class="border border-gray-400 rounded block p-2 w-full text-base"
+          class="border border-gray-400 rounded block py-1 px-2 w-full text-base"
           type="text"
-          v-model="text"
+          v-model="fields.text"
           id="text"
+          required
           placeholder="Enter text..."
         />
+        <span class="text-xs text-custom-red">{{ fieldErrors.text }}</span>
       </div>
       <div class="">
         <label class="my-4 inline-block font-medium" for="amount"
@@ -20,15 +22,19 @@
           (negative - expense, positive - income)</label
         >
         <input
-          class="border border-gray-400 rounded block p-2 w-full"
+          class="border border-gray-400 rounded block py-1 px-2 w-full block"
           type="number"
           id="amount"
-          v-model.number="amount"
+          v-model.number="fields.amount"
+          required
           placeholder="Enter amount..."
         />
+        <span class="text-xs text-custom-red block">{{
+          fieldErrors.amount
+        }}</span>
       </div>
       <button
-        class="font-medium bg-purple-700 shadow-md rounded-md text-white block p-2 my-4 w-full hover:bg-purple-600 focus:outline-none"
+        class="font-normal bg-purple-700 shadow-md rounded-md text-white block py-1 my-4 w-full hover:bg-purple-600 focus:outline-none"
       >
         Add Transaction
       </button>
@@ -40,19 +46,41 @@ export default {
   name: "AddTransaction",
   data() {
     return {
-      text: "",
-      amount: 0
+      fields: {
+        text: "",
+        amount: 0
+      },
+      fieldErrors: {
+        text: null,
+        amount: null
+      }
     };
   },
 
   methods: {
     addTransaction() {
+      this.fieldErrors = this.validateForm(this.fields);
+      if (Object.keys(this.fieldErrors).length) return;
+
       let newTransaction = {
         id: Date.now(),
-        text: this.text,
-        amount: this.amount
+        text: this.fields.text,
+        amount: this.fields.amount
       };
       this.$store.dispatch("addTransaction", newTransaction);
+
+      this.fields.text = "";
+      this.fields.amount = 0;
+    },
+
+    validateForm(fields) {
+      const errors = {};
+      if (!fields.text) errors.text = "text cannot be empty";
+      if (fields.text && fields.text.length < 3)
+        errors.text = "text name must be greater than 2";
+      if (fields.amount == 0) errors.amount = "amount cannot be 0";
+
+      return errors;
     }
   }
 };
